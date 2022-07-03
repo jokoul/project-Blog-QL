567459244782db6d1cf6c7b06349cecb4f77d14f
jokoul <74833985+jokoul@users.noreply.github.com>
2022-07-03 11:13:22 +0200

    end of project project without deployment

---
+++ functions/graphql/resolvers.js
@@ -0,0 +1,20 @@
+const firebase = require("firebase-admin");
+
+const resolvers = {
+    Query: {
+        async hello(_, { name }) {
+            return `Hello ${name || "World"}`;
+        },
+    },
+
+    Mutation: {
+        async addTodo(_, { text }) {
+            const db = firebase.firestore();
+            const ref = db.collection("todos").doc();
+            await ref.set({ text });
+            return ref.id;
+        },
+    },
+};
+
+module.exports = resolvers;
---
+++ functions/graphql/schema.js
@@ -0,0 +1,12 @@
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+
+  type Mutation {
+    addTodo(text: String): String!
+  }
+
+`;
+
+module.exports = typeDefs;
